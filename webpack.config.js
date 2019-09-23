11ec0584e2dc6e565ac2f0caa61649431c35bd37
Josué Gimenes <josuegalvares@gmail.com>
2019-09-23 12:38:40 -0300

    Importing CSS

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,14 @@ module.exports = {
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
-                },
-            },
+                }
+            }, {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                ]
+            }
         ],
     },
 };
