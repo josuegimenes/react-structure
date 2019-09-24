77549f169564b5ff7fadd3b76e2fd715ddfb0335
Josué Gimenes <josuegalvares@gmail.com>
2019-09-24 10:00:56 -0300

    Removing State Items

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -29,12 +29,23 @@ class TechList extends Component {
         });
     }
 
+    handleDelete = (tech) => {
+        //console.log(tech);
+
+        this.setState({ techs: this.state.techs.filter(t => t !== tech ) })
+    }
+
     render() {
         return (
             <form onSubmit={this.handleSubmit} >
                 <h1>{this.state.newTech}</h1>
                 <ul>
-                    {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+                    {this.state.techs.map(tech => (
+                        <li key={tech}>
+                            {tech}
+                            <button onClick={() => this.handleDelete(tech)} type="button">Remover</button>
+                        </li>
+                    ))}
                 </ul>
                 <input 
                     type="text" 
